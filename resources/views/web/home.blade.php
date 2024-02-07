fb0b8a78fd732d43191778ad23ab395fba47470d
sebastian8080 <sebas25211@hotmail.com>
2024-02-07 15:38:24 -0500

    cambiando imagenes banner de la home

--- resources/views/web/home.blade.php
+++ resources/views/web/home.blade.php
@@ -137,7 +137,7 @@
             </div>
             <div style="height: 750px;" class="carousel-inner min-height-banner">
                 <div class="carousel-item position-relative active">
-                    <img height="750px" style="object-fit: @if($mobile) contain @else cover @endif; object-position: center" src="@if($mobile) {{ asset('img/banner1-mobile.jpg') }} @else {{ asset('img/banner1.jpg') }} @endif" class="d-block w-100 min-height-banner" alt="Creditos en Ecuador">
+                    <img height="750px" style="object-fit: @if($mobile) contain @else cover @endif; object-position: center" src="@if($mobile) {{ asset('img/banner1-mobile.jpg') }} @else {{ asset('img/bannerhome1.jpg') }} @endif" class="d-block w-100 min-height-banner" alt="Creditos en Ecuador">
                     <div class="position-absolute min-height-banner left-10px text-banner" style="top: 0; left: 220px; height: 750px; display: flex; align-items: center;">
                         <div data-aos="fade-right">
                             <h1 style="font-family: 'Bitter', 'serif'; color: #676667"><span style="font-size: 3rem; font-style: italic" class="f-size-min-2">Accede a créditos en</span> <br> <span style="font-size: 7rem; font-weight: 600" class="f-size-min-3">Ecuador</span> <br> <span style="font-size: 3rem; font-style: italic" class="f-size-min-2">desde Estados Unidos</span> </h1>
@@ -146,7 +146,7 @@
                     </div>
                 </div>
                 <div class="carousel-item position-relative">
-                    <img height="750px" style="object-fit: @if($mobile) contain @else cover @endif; object-position: center" src="@if($mobile){{ asset('img/banner2-mobile.jpg') }} @else {{ asset('img/banner2.jpg') }} @endif" class="d-block w-100 min-height-banner" alt="Creditos Hipotecarios en Ecuador">
+                    <img height="750px" style="object-fit: @if($mobile) contain @else cover @endif; object-position: center" src="@if($mobile){{ asset('img/banner2-mobile.jpg') }} @else {{ asset('img/bannerhome2.jpg') }} @endif" class="d-block w-100 min-height-banner" alt="Creditos Hipotecarios en Ecuador">
                     <div class="position-absolute min-height-banner left-10px text-banner" style="top: 0; left: 220px; height: 750px; display: flex; align-items: center;">
                         <div>
                             <h2 style="font-family: 'Bitter', 'serif'; color: #676667"><span style="font-size: 3rem; font-style: italic" class="f-size-min-2">Adquiere el hogar que</span> <br> <span style="font-size: 7rem; font-weight: 600" class="f-size-min-3">Mereces</span> <br> <span style="font-size: 3rem; font-style: italic" class="f-size-min-2">con un crédito hipotecario</span> </h2>
@@ -155,7 +155,7 @@
                     </div>
                 </div>
                 <div class="carousel-item position-relative">
-                    <img height="750px" style="object-fit: @if($mobile) contain @else cover @endif; object-position: center" src="@if($mobile){{ asset('img/banner3-mobile.jpg') }} @else {{ asset('img/banner3.jpg') }} @endif" class="d-block w-100 min-height-banner" alt="Creditos de Consumo en Ecuador">
+                    <img height="750px" style="object-fit: @if($mobile) contain @else cover @endif; object-position: center" src="@if($mobile){{ asset('img/banner3-mobile.jpg') }} @else {{ asset('img/bannerhome3.jpg') }} @endif" class="d-block w-100 min-height-banner" alt="Creditos de Consumo en Ecuador">
                     <div class="position-absolute min-height-banner left-10px text-banner" style="top: 0; left: 220px; height: 750px; display: flex; align-items: center;">
                         <div>
                             <h2 style="font-family: 'Bitter', 'serif'; color: #676667"><span style="font-size: 3rem; font-style: italic" class="f-size-min-2">Compra el carro de</span> <br> <span style="font-size: 7rem; font-weight: 600" class="f-size-min-3">Tus sueños</span> <br> <span style="font-size: 3rem; font-style: italic" class="f-size-min-2">con un crédito de consumo</span> </h2>
