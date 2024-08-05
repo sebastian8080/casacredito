de78383058be5288f4cbef819e31ba120eb43c83
sebastian8080 <sebas25211@hotmail.com>
2024-08-05 11:15:29 -0500

    cambiando numero de telefono

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -228,7 +228,7 @@
                             <article class="d-flex align-items-center">
                                 <img width="30px" height="30px" src="{{ asset('img/call-icon.png') }}" alt="">
                                 <div>
-                                    <span><a style="text-decoration: none; color: #676667" href="tel:+593958959084">095-895-9084</a></span>
+                                    <span><a style="text-decoration: none; color: #676667" href="tel:+593989571348">098-957-1348</a></span>
                                     {{-- tel:+593958959084 --}}
                                 </div>
                             </article>
