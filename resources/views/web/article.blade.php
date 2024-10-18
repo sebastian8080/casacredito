ab2bddce2da21203c6d54c1472b765c5f510e561
sebastian8080 <sebas25211@hotmail.com>
2024-10-18 15:28:17 -0500

    feat: add meta tags to articles in the blog

--- resources/views/web/article.blade.php
+++ resources/views/web/article.blade.php
@@ -3,13 +3,24 @@
 @section('head')
 
     <title>{{ $article->title }}</title>
+    <meta name="description" content="{{ $article->metadescription}}">
+
+    <meta name="robots" content="INDEX,FOLLOW,SNIPPET">
+    <link rel="canonical" href="{{ Request::url() }}">
+
+    <!-- Open Graph / Facebook -->
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="{{ Request::url() }}" />
+    <meta property="og:title" content="{{ $article->title }}" />
+    <meta property="og:description" content="{{ $article->metadescription }}" />
+    <meta property="og:image" content="{{ asset('uploads/webp/'.$article->banner_image) }}" />
 
     <style>
         body{
             background-color: #ffffff !important;
         }
 
-        @media screen and (max-width: 580px){
+        /* @media screen and (max-width: 580px){
             .height-banner-mobile{
                 height: 550px !important;
             }
@@ -48,25 +59,64 @@
             .contenedor-articles{
                 display: inline !important;
             }
+        } */
+        /* Estilos para la capa de overlay */
+        .bg-image-banner-mobile {
+            position: relative;
+        }
+
+        .bg-image-banner-mobile .overlay {
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            background-color: rgba(0, 0, 0, 0.5); /* Capa oscura */
+            z-index: 1;
+        }
+
+        .height-banner-mobile .d-flex {
+            position: relative;
+            z-index: 2; /* El texto debe estar por encima de la capa */
+        }
+
+        /* Ajustes para móviles */
+        @media screen and (max-width: 580px){
+            .height-banner-mobile {
+                height: 550px !important;
+            }
+            .bg-image-banner-mobile {
+                background-position: center !important;
+            }
+            .f-size-min-2 {
+                font-size: 1.4rem !important;
+            }
+            .padding-left-banner-mobile {
+                padding: 0 10px !important;
+            }
         }
     </style>
 
 @endsection
 
 @section('content')
-    <section class="height-banner-mobile bg-image-banner-mobile" style="height: 750px; background-position: center; background-repeat: no-repeat; background-size: cover; background-image: url('{{ asset('uploads/webp/'.$article->banner_image) }}')">
+    <section class="height-banner-mobile bg-image-banner-mobile" style="position: relative; height: 750px; background-position: center; background-repeat: no-repeat; background-size: cover; background-image: url('{{ asset('uploads/webp/'.$article->banner_image) }}')">
+        <!-- Agregamos un div para el overlay -->
+        <div class="overlay"></div>
         <section class="height-banner-mobile padding-left-banner-mobile d-flex align-items-center justify-content-center" style="height: 750px;">
             <div data-aos="fade-right">
-                <h1 style="font-family: 'Bitter', 'serif'; color: #ffffff; font-weight: 600"><span style="font-size: 3rem; font-style: italic" class="f-size-min-2">{{ $article->title }}</span> </h1>
+                <h1 style="font-family: 'Bitter', 'serif'; color: #ffffff; font-weight: 600">
+                    <span style="font-size: 3rem; font-style: italic" class="f-size-min-2">{{ $article->h1 ? $article->h1 : $article->title }}</span>
+                </h1>
+                @if($article->metadescription != null)
+                    <p class="text-center text-white">{{ $article->metadescription }}</p>
+                @endif
             </div>
         </section>
     </section>
 
     <section class="container pt-5 pb-5">
-        <section class="bg-light px-4 pt-3 border shadow-sm rounded">
-            <p style="font-size: 18px">{{ $article->metadescription }}</p>
-        </section>
-        <section class="mt-5 row">
+        <section class="mt-4 row">
             <div class="col-sm-8">
                 {!! $article->body_html !!}
             </div>
